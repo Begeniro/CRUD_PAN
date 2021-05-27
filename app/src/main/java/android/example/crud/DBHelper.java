417479c0a3cebb516bc24eb7f2b6a5149f00e1f9
Begeniro <61041689+Begeniro@users.noreply.github.com>
2021-05-27 22:27:42 +0700

    Update

--- app/src/main/java/android/example/crud/DBHelper.java
+++ app/src/main/java/android/example/crud/DBHelper.java
@@ -13,33 +13,34 @@ import java.util.Map;
 
 public class DBHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
-    private static String DATABASE_NAME = "book_db";
+    public static String DATABASE_NAME = "book_db";
     private static final String TABLE_BUKU = "book";
     private static final String KEY_ID_BUKU = "id_buku";
     private static final String KEY_JUDUL = "judul";
     private static final String KEY_HARGA = "harga";
 
-    private static final String CREATE_DATABASE_BUKU = "CREATE TABLE "+ TABLE_BUKU + "("
+    private static final String CREATE_TABLE_BUKU = "CREATE TABLE " + TABLE_BUKU + "("
             + KEY_ID_BUKU + " INTEGER PRIMARY KEY AUTOINCREMENT,"
             + KEY_JUDUL + " TEXT,"
             + KEY_HARGA + " INTEGER)";
 
-    public DBHelper(Context context){
-        super (context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_DATABASE_BUKU);
+        //mengeksekusi perintah create tabel transaksi
+        db.execSQL(CREATE_TABLE_BUKU);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS '" + TABLE_BUKU + "'");
+        onCreate(db);
     }
 
-    public void addBuku(String judul, int harga){
+    public void addBuku(String judul, int harga) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(KEY_JUDUL, judul);
@@ -48,29 +49,26 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_BUKU, null, values);
     }
 
-    public ArrayList<Map<String,String>> getBuku(){
+    public ArrayList<Map<String, String>> getBuku() {
         ArrayList<Map<String, String>> arrayList = new ArrayList<>();
-        int id_buku= 0;
+        int id_transaksi = 0;
         String judul = "";
         int harga = 0;
 
-        String selectQuery = "SELECT * FROM "+ TABLE_BUKU;
+        String selectQuery = "SELECT * FROM " + TABLE_BUKU;
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor c = db.rawQuery(selectQuery, null);
-        if (c.moveToFirst()){
+        if (c.moveToFirst()) {
             do {
-                id_buku = c.getInt(c.getColumnIndex(KEY_ID_BUKU));
+                id_transaksi = c.getInt(c.getColumnIndex(KEY_ID_BUKU));
                 judul = c.getString(c.getColumnIndex(KEY_JUDUL));
                 harga = c.getInt(c.getColumnIndex(KEY_HARGA));
-
                 Map<String, String> itemMap = new HashMap<>();
-
-                itemMap.put(KEY_ID_BUKU, String.valueOf(id_buku));
+                itemMap.put(KEY_ID_BUKU, String.valueOf(id_transaksi));
                 itemMap.put(KEY_JUDUL, judul);
                 itemMap.put(KEY_HARGA, String.valueOf(harga));
-
                 arrayList.add(itemMap);
-            }while (c.moveToNext());
+            } while (c.moveToNext());
         }
         return arrayList;
     }
--- app/src/main/java/android/example/crud/MainActivity.java
+++ app/src/main/java/android/example/crud/MainActivity.java
@@ -2,6 +2,7 @@ package android.example.crud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -12,46 +13,55 @@ import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<Map<String, String>> arrayList;
     EditText judul, harga;
     Button tambah;
-
     DBHelper dbcenter;
-
-    ArrayList<Map<String, String>> arrayList;
-    ListView listView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        listView=findViewById(R.id.listView);
+        dbcenter = new DBHelper(this);
         tambah = findViewById(R.id.button);
+
         judul = findViewById(R.id.editJudul);
         harga = findViewById(R.id.editHarga);
 
-        tambah.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+
+        tambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 SQLiteDatabase db = dbcenter.getWritableDatabase();
                 dbcenter.addBuku(
                         judul.getText().toString(),
-                        Integer.parseInt(harga.getText().toString())
-                );
+                        Integer.parseInt(harga.getText().toString()));
                 onResume();
+
             }
         });
     }
 
     private void loadData(){
-        arrayList = dbcenter.getBuku();
+        arrayList=dbcenter.getBuku();
 
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this,arrayList,
-                android.R.layout.simple_expandable_list_item_2, new String[]{"judul, harga"},
-                new int[]{android.R.id.text1, android.R.id.text2});
+        Collections.sort(arrayList, new Comparator<Map<String, String>>() {
+            @Override
+            public int compare(Map<String, String> t1, Map<String, String> t2) {
+                return t2.get("id_buku").compareTo(t1.get("id_buku"));
+            }
+        });
 
+        SimpleAdapter simpleAdapter=new SimpleAdapter(this, arrayList,
+                android.R.layout.simple_expandable_list_item_2,new String[]{"judul","harga"},
+                new int[]{android.R.id.text1,android.R.id.text2});
         listView.setAdapter(simpleAdapter);
         simpleAdapter.notifyDataSetChanged();
     }
